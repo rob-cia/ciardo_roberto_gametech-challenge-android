9699a226a476f6ec9daa306df817b628c996d7c3
rob-cia <roberto.ciardo@studenti.polito.it>
2024-11-23 20:26:22 +0100

    Implementation of Schedule Notifications
    - creation of titles, descriptions, icons
    - scheduleNotifications: create an alarmManager -> create an Intent -> create a pendingIntent -> set alarm

--- ciardo_roberto_gametech-challenge-android/src/main/java/com/rc/ciardo_roberto_gametech_challenge_android/NotificationPlugin.java
+++ ciardo_roberto_gametech-challenge-android/src/main/java/com/rc/ciardo_roberto_gametech_challenge_android/NotificationPlugin.java
@@ -1,9 +1,70 @@
 package com.rc.ciardo_roberto_gametech_challenge_android;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 public class NotificationPlugin {
-    public static void scheduleNotifications() {
-        Log.d("NotificationPlugin", "OnClick: Send notifications");
+
+    private static final String TAG = "NotificationPlugin";
+    private static final int NOTIFICATION_ID_BASE = 1000;
+    private static final int NOTIFICATION_COUNT = 5;
+    private static final long INTERVAL_MS = 10 * 1000;
+
+    private static final String[] TITLES = {
+            "Notification 1",
+            "Notification 2",
+            "Notification 3",
+            "Notification 4",
+            "Notification 5"
+    };
+
+    private static final String[] DESCRIPTIONS = {
+            "Content of the notification 1",
+            "Content of the notification 2",
+            "Content of the notification 3",
+            "Content of the notification 4",
+            "Content of the notification 5"
+    };
+
+    private static final int[] ICONS = {
+            android.R.drawable.ic_notification_overlay,
+            android.R.drawable.ic_menu_camera,
+            android.R.drawable.ic_menu_compass,
+            android.R.drawable.ic_menu_help,
+            android.R.drawable.ic_menu_info_details
+    };
+
+    public static void scheduleNotifications(Context context) {
+        Log.d(TAG, "scheduleNotifications: Scheduling " + NOTIFICATION_COUNT + " notifications...");
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        if (alarmManager == null) {
+            Log.e(TAG, "scheduleNotifications: AlarmManager is null!");
+            return;
+        }
+
+        for (int i = 0; i < NOTIFICATION_COUNT; i++) {
+            Intent intent = new Intent(context, NotificationReceiver.class);
+            intent.putExtra("notificationId", NOTIFICATION_ID_BASE + i);
+            intent.putExtra("title", TITLES[i]);
+            intent.putExtra("description", DESCRIPTIONS[i]);
+            intent.putExtra("icon", ICONS[i]);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    NOTIFICATION_ID_BASE + i,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE // Android 12+
+            );
+
+            long triggerTime = SystemClock.elapsedRealtime() + ((i+1) * INTERVAL_MS);
+            alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, pendingIntent);
+
+            Log.d(TAG, "scheduleNotifications: Scheduled notification " + (i + 1) + " at " + triggerTime);
+        }
     }
 }
